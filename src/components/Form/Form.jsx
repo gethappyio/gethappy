fe0d7eb0b820d38a0671697fbda0669a60fac408
adamatronix <adamgeework@gmail.com>
2018-09-03 13:29:32 +0800

    GH-Input-text Change input text syntax

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles/form-field.scss";
 import "./styles/form-text.scss";
 
+class InputText extends Component {
+    constructor() {
+        super();
+    }
 
-const InputText = ({ field, form, ...props }) => (
-   <div className="form-field__wrapper">
-        <input {...field} {...props} className="form-text"/>
-   </div>
-);
+    render() {
+        const { field, form: { touched, errors }, ...props } = this.props;
+        return(
+            <div className="form-field__wrapper">
+                <input {...field} {...props} className="form-text"/>
+            </div>
+        );
+    }
+    
+}
 
 export { InputText };
