1d08a86703f840bd834c4d026a0e2905b0a26fad
adamatronix <adamgeework@gmail.com>
2018-09-07 17:26:33 +0800

    GH-108 Setup base service module

---
+++ modules/gethappy/src/GethappyModule.php
@@ -0,0 +1,12 @@
+<?php
+namespace modules\gethappy;
+
+class GethappyModule extends \yii\base\Module
+{
+    public function init()
+    {
+        parent::init();
+
+        // Custom initialization code goes here...
+    }
+}
