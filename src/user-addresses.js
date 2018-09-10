2ba84bd94bb1f307b3e1094976ac7c17975941f0
adamatronix <adamgeework@gmail.com>
2018-09-10 17:20:12 +0800

    GH-112 Add address page

--- src/user-addresses.js
+++ src/user-addresses.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import AddressCard from "./components/AddressCard/AddressCard";
+import Addresses from "./components/Addresses/Addresses";
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+
+import brandonWoff from './assets/fonts/brandon_reg-webfont.woff';
+import brandonWoff2 from './assets/fonts/brandon_reg-webfont.woff2';
+import brandonEot from './assets/fonts/brandon_reg-webfont.eot';
+import brandonTtf from './assets/fonts/brandon_reg-webfont.ttf';
+import brandonSvg from './assets/fonts/brandon_reg-webfont.svg';
+
+import "normalize.css";
+import "./styles/app.scss";
 
 ReactDOM.render(
-    window.customerAddressData.map((address) => 
-        <AddressCard address={address} />
-    ), 
-    document.getElementById("userAddresses"));
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/user/addresses' component={Addresses} />
+        </Switch>
+    </BrowserRouter>, 
+    document.getElementById("app"));
