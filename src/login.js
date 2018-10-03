5f2a478aef76be76d645985c3eb3263934c2ad69
adamatronix <adamgeework@gmail.com>
2018-10-03 11:44:12 +0800

    GH-4 Add a base narrow container to limit how big containers can get on desktop

--- src/login.js
+++ src/login.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+import Page from "./components/Page/Page";
 import LoginForm from "./components/LoginForm/LoginForm";
 import NewUserForm from "./components/NewUserForm/NewUserForm";
 
@@ -13,6 +15,14 @@ var newUserForm = document.getElementById("newUserForm");
 newUserForm ? ReactDOM.render(<NewUserForm />, newUserForm) : false;
 
 ReactDOM.render(
-    <div><LoginForm />
-    <NewUserForm /></div>,
+    <BrowserRouter>
+        <Page>
+            <div class="base__pad">
+                <div className="base__narrow">
+                    <LoginForm />
+                    <NewUserForm />
+                </div>
+            </div>
+        </Page>
+    </BrowserRouter>,
     document.getElementById("app"));
