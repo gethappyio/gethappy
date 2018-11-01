a554996bcfef17069938baad76d771e8d2b5f95e
adamatronix <adamgeework@gmail.com>
2018-11-01 16:16:47 +0800

    GH-125 Add more experience styling with new font

--- src/components/FeaturedExperience/FeaturedExperience.jsx
+++ src/components/FeaturedExperience/FeaturedExperience.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import BtnPrimary from "../BtnPrimary/BtnPrimary";
 import "./styles/featured-experience.scss";
 import { Link } from "react-router-dom";
 
@@ -13,26 +14,26 @@ class FeaturedExperience extends Component {
   render() {
         let data = this.props.data;
         return (
-            <Link to={data.uri} className="featured-experience">
+            <div className="featured-experience">
                 <div className="featured-experience__top">
-                    <div className="featured-experience__title-wrapper">
-                        <h1 className="featured-experience__title">{data.title}</h1>
+                    <div className="featured-experience__days">
+                        <div className="featured-experience__days--number">{data.days}</div>
+                        <div className="featured-experience__days--left">days left!</div>
                     </div>
                     <img className="featured-experience__image" src={data.featuredImage} />
                 </div>
                 <div className="featured-experience__bottom section__wrapper">
                     <div className="featured-experience__bottom-inner section__col-xs-12">
-                        <h2 className="featured-experience__description">
-                            <span className="featured-experience__description-top">{data.descTop}</span>
-                            <span className="featured-experience__description-bottom">{data.descBottom}</span>
+                        <h2 className="featured-experience__title">
+                            {data.title}
                         </h2>
-                        <div className="section__wrapper">
-                            <div className="featured-experience__days section__col-xs-6">20 days left</div>
-                            <div className="featured-experience__more section__col-xs-6">learn more</div>
-                        </div>
+                        <p className="featured-experience__description">{data.desc}</p>
+                        <Link to={data.uri} className="featured-experience__link">
+                            <BtnPrimary className="btn-primary--vibrant-blue-outline">Learn more</BtnPrimary>
+                        </Link>
                     </div>
                 </div>
-            </Link>
+            </div>
         );
   }
 }
