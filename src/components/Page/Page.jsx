49ae8b72a132cd1c00c07f57402162a20c97cd9a
adamatronix <adamgeework@gmail.com>
2018-11-13 15:26:57 +0800

    GH-11 Add back page

---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import classNames from 'classnames/bind';
+
+class Page extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          title: ""
+        };
+      }
+    
+      render() {
+        let {className} = this.props;
+        let classes = classNames({
+            "base__expand": true,
+            "base__withNav": this.props.transparentNav ? false : true
+        }, className);
+
+        let nav = this.props.navigation;
+        let footer = this.props.footer;
+        let noNav = this.props.noNav;
+
+        if(!nav) {
+            nav = <Header />;
+        }
+
+        if(footer === "false") {
+            footer = "";
+        } else {
+            footer = <Footer />
+        }
+
+        return (
+            <main className={classes}>
+                {noNav ? " " : nav}
+                <div class="base__expand base__scroll">
+                    {this.props.children}
+                    {footer}
+                </div>
+            </main>
+        );
+      }
+}
+
+export default Page;
