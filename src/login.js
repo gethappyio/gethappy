5f68fe3746b1326d6a546d0e43637b7630b8652d
adamatronix <adamgeework@gmail.com>
2018-11-14 15:57:18 +0800

    Quick-fixes Add checkout header to login

--- src/login.js
+++ src/login.js
@@ -3,15 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route} from "react-router-dom";
 import Page from "./components/Page/Page";
 import LoginForm from "./components/LoginForm/LoginForm";
-import FacebookLogin from "./components/FacebookLogin/FacebookLogin";
 import NewUserForm from "./components/NewUserForm/NewUserForm";
-
+import CheckoutBar from "./components/Checkout/CheckoutBar";
 import "normalize.css";
 import "./styles/app.scss";
 
 ReactDOM.render(
     <BrowserRouter>
-        <Page footer="false">
+        <Page navigation={<CheckoutBar title="Checkout" />} footer="false">
             <div class="base__pad">
                 <div className="base__narrow base__margin-top">
                     <LoginForm />
