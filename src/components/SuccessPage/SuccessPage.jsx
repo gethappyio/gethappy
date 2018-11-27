e3d941e2c8d6f863991c2871e1fe26a764a06417
adamatronix <adamgeework@gmail.com>
2018-11-27 16:02:24 +0800

    Success Add share module from experiences Page

--- src/components/SuccessPage/SuccessPage.jsx
+++ src/components/SuccessPage/SuccessPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Page from "../Page/Page";
 import CelebrateCanvas from "../CelebrateCanvas/CelebrateCanvas";
+import ExperienceShare from "../Experience/ExperienceShare";
 import iconCheck from "./assets/checkmark.svg";
 import "./styles/success.scss";
 
@@ -19,11 +20,12 @@ class Checkout extends Component {
         return (
             <Page noNav="true" transparentNav="true" footer="false">
                 <CelebrateCanvas />
-                <div className="section__wrapper success__wrapper">
+                <div className="base__narrow section__wrapper success__wrapper">
                     <div className="section__col-xs-12">
                         <img className="success__checkmark" src={iconCheck} />
                         <h2 className="success__header">Perfect. <br/> You're done!</h2>
                         <p className="success__sub">Thanks for saving the world</p>
+                        <ExperienceShare />
                     </div>
                     <div className="success__continue">
                         <a href="/" className="success__continue-text">Continue making others happy</a>
