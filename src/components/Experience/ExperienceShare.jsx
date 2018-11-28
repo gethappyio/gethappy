010ba3cc4e383e5b4fbe99f1e5952668ff1027d9
adamatronix <adamgeework@gmail.com>
2018-11-28 14:42:29 +0800

    Share Add module wit fb, twitter, and native share

--- src/components/Experience/ExperienceShare.jsx
+++ src/components/Experience/ExperienceShare.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
+import { FacebookShareButton, TwitterShareButton } from "react-share";
 import "./styles/experience-share.scss";
 import iconLink from "./assets/link.svg";
 import iconFb from "./assets/fb.svg";
 import iconTwitter from "./assets/twitter.svg";
 
-
 class ExperienceShare extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {};
+
+        this.nativeShare = this.nativeShare.bind(this);
+    }
+
+    nativeShare() {
+        navigator.share({
+            title: 'Get Happy experience',
+            text: this.props.quote,
+            url: this.props.url
+        })
+    }
+
+    navigatorShare() {
+        if (navigator.share) {
+            return (
+            <div>
+                <img className="experience-share__icon" onClick={this.nativeShare} src={iconLink} />
+            </div>);
+        } else {
+            return "";
+        }
     }
 
     render() {
@@ -19,9 +40,17 @@ class ExperienceShare extends Component {
                     <div className="experience-share__line"></div>
                     <div className="experience-share__inner">
                         <div><h2 className="experience-share__title">share me!</h2></div>
-                        <div><img className="experience-share__icon" src={iconLink} /></div>
-                        <div><img className="experience-share__icon" src={iconFb} /></div>
-                        <div><img className="experience-share__icon" src={iconTwitter} /></div>
+                        {this.navigatorShare()}
+                        <div>
+                            <FacebookShareButton quote={this.props.quote} url={this.props.url}>
+                                <img className="experience-share__icon" src={iconFb} />
+                            </FacebookShareButton>
+                        </div>
+                        <div>
+                            <TwitterShareButton title={this.props.quote} url={this.props.url}>
+                                <img className="experience-share__icon" src={iconTwitter} />
+                            </TwitterShareButton>
+                        </div>
                     </div>
                     <div className="experience-share__line"></div>
                 </div>
