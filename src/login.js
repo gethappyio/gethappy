9756d76a5113cdbffcde96128f2c86714e60fb76
adamatronix <adamgeework@gmail.com>
2019-01-04 17:01:13 +0800

    Emails Add activation email

--- src/login.js
+++ src/login.js
@@ -4,13 +4,15 @@ import { BrowserRouter, Switch, Route} from "react-router-dom";
 import Page from "./components/Page/Page";
 import LoginForm from "./components/LoginForm/LoginForm";
 import NewUserForm from "./components/NewUserForm/NewUserForm";
-import CheckoutBar from "./components/Checkout/CheckoutBar";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
 import "normalize.css";
 import "./styles/app.scss";
 
 ReactDOM.render(
     <BrowserRouter>
-        <Page navigation={<CheckoutBar title="Checkout" />} footer="false">
+        <Page navigation={
+            <NavigationBar title="Login" href="/user" /> 
+        } footer="false">
             <div class="base__pad">
                 <div className="base__narrow base__margin-top">
                     <LoginForm />
