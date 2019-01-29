4d473fb77f835449371ffe583a4e49ca0653daea
adamatronix <adamgeework@gmail.com>
2019-01-29 17:25:17 +0800

    GH-13 Use custom play and pause for video tag

--- src/components/Experience/ExperienceVideo.jsx
+++ src/components/Experience/ExperienceVideo.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./styles/experience-video.scss";
+import { throws } from "assert";
+import { timingSafeEqual } from "crypto";
 
 class ExperienceVideo extends Component {
 
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            pause: true
+        };
     }
 
     openFullScreen() {
@@ -17,7 +21,26 @@ class ExperienceVideo extends Component {
         else if (video.webkitRequestFullscreen){
             video.webkitRequestFullscreen();
         }
+        else if (video.webkitEnterFullScreen) {
+            // Toggle fullscreen in Safari for iPad
+            video.webkitEnterFullScreen();
+        }
 
+    }
+
+    togglePlay() {
+        var video = document.getElementById('experience-video');
+        if(video.paused) {
+            video.play();
+            this.setState({
+                pause: false
+            });
+        } else {
+            video.pause();
+            this.setState({
+                pause: true
+            });
+        }
     }
 
     render() {
@@ -26,9 +49,13 @@ class ExperienceVideo extends Component {
         return (
             <div className="experience-video__wrapper">
                 <div className="experience-video__play-container">
-                    <img className="experience-video__play" src={window.cloudfront + "play-btn.svg"} />
-                </div> 
-                <img className="experience-video__thumb" src={poster} onClick={this.openFullScreen}/>
+                {this.state.pause ? <img className="experience-video__play" src={window.cloudfront + "play-btn.svg"} /> : ""}
+                </div>
+                <video id="experience-video" width="100%" poster={poster} onClick={this.togglePlay.bind(this)}>
+                    <source src={src} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                { /*<img className="experience-video__thumb" src={poster} onClick={this.openFullScreen}/> */}
             </div>
         );
     }
